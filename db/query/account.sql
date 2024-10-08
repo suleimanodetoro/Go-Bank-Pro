bf5e397dcd7ae0a0cfd34bb0edcbbb48b0723157
Suleiman Odetoro <odetoro75@gmail.com>
2024-10-08 19:11:53 +0100

    Implement simpler balance update in db store

--- db/query/account.sql
+++ db/query/account.sql
@@ -28,8 +28,8 @@ RETURNING *;
 
 -- name: AddAccountBalance :one
 UPDATE accounts
-SET balance = balance + $2
-WHERE id = $1
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
