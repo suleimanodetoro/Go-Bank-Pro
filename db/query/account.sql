c3017883892410e4237360820263496ba9f5c162
Suleiman Odetoro <odetoro75@gmail.com>
2024-10-08 18:07:02 +0100

    Add new query for more efficient account balance updates

--- db/query/account.sql
+++ db/query/account.sql
@@ -26,6 +26,12 @@ SET balance = $2
 WHERE id = $1
 RETURNING *;
 
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + $2
+WHERE id = $1
+RETURNING *;
+
 -- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1;
